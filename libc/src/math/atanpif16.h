71770931c77c8c8241ac6b3ad743f5d4d8b2d567
Brighht <bright.andohh@gmail.com>
2025-04-01 20:54:03 -0500

    Add atanpi16 header for _Float16 support

---
+++ libc/src/math/atanpif16.h
@@ -0,0 +1,13 @@
+//===-- Implementation header for atanpif16 ---------------*- C++ -*-===//
+#ifndef LLVM_LIBC_SRC_MATH_ATANPIF16_H
+#define LLVM_LIBC_SRC_MATH_ATANPIF16_H
+
+#include "src/__support/macros/config.h"
+
+namespace LIBC_NAMESPACE {
+
+_Float16 atanpif16(_Float16 x);
+
+} 
+
+#endif
